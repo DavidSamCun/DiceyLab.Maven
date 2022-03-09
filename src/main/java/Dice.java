d501e54fadd4c40fd53c50f8d8fa1f31dcefbe2d
David <davidsamcun@gmail.com>
2022-03-08 20:09:01 -0500

    test

--- src/main/java/Dice.java
+++ src/main/java/Dice.java
@@ -1,4 +1,11 @@
+import java.util.Random;
+import java.util.Scanner;
+import java.util.Arrays;
+
 public class Dice {
+
+    Random rnd = new Random();
+    private int mystery = rnd.nextInt(1,6);
 
 
 }
