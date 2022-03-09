f349bd61cf0a538fe734f2d6c17d222bf85370c4
DavidCun <99755247+DavidSamCun@users.noreply.github.com>
2022-03-09 16:40:31 -0500

    test

--- src/main/java/Dice.java
+++ src/main/java/Dice.java
@@ -1,11 +1,48 @@
 import java.util.Random;
-import java.util.Scanner;
-import java.util.Arrays;
 
 public class Dice {
 
-    Random rnd = new Random();
-    private int mystery = rnd.nextInt(1,6);
+    private static Random rand = new Random();
+    private static int dice;      //Default number of dice
+    private static int defMax = 1;
+    private static int defMin = 6;       //Default min
+    //private final int numOfDie;
+    //private int rollResult;
+
+    //Default constructor
+
+    public Dice(){
+        this.dice = 1;
+    }
+
+    public Dice(int amnt){
+        if(amnt<0){
+            System.out.println("Need an amount greater than 0");
+        } else {
+            this.dice = amnt;
+        }
+    }
+
+    public void removeDie(){//Setter
+        if (this.dice > 0) {
+            this.dice--;
+        } else {
+            System.out.println("Amount can't be less than 1");
+        }
+    }
+
+    public void addDice(){               //Setter
+        this.dice++;
+    }
+
+    public int getAmntDice(){            //Getter
+        return this.dice;
+    }
+
+    public int tossAndSum(){             //Getter
+        int output = rand.nextInt(1*dice, 6*dice);
+        return output;
+    }
 
 
 }
