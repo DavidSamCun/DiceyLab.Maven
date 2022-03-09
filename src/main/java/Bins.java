59714d08b22f9e520e774d83ab7d892b40968918
DavidCun <99755247+DavidSamCun@users.noreply.github.com>
2022-03-08 22:57:03 -0500

    workinOnBin

--- src/main/java/Bins.java
+++ src/main/java/Bins.java
@@ -5,3 +5,17 @@ public class Bins {
 
 
 }
+
+
+/*
+
+idea is to get bins that have an array list of ints. and each int index would increment based on roll
+
+    example
+            Bins results = new Bins( 2 the min roll, 12 the max roll)
+
+
+                    Integer number of tens ====== results.getBin(10) // returns the number of times 10 has been roled
+                    results.incrementBin 10  ==== increment bin 10 by 1 if rolled a 10
+
+*/
