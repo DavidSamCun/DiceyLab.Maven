7f8b49218c7bdc2db0b40cfe688b5957246fcfe2
DavidCun <99755247+DavidSamCun@users.noreply.github.com>
2022-03-11 14:49:18 -0500

    lineItUp

--- src/main/java/Simulation.java
+++ src/main/java/Simulation.java
@@ -5,6 +5,7 @@ public class Simulation {
     Dice dice;
     Bins bin;
     int tries;
+    String rollResult;
 
     public Simulation(){
 
@@ -23,6 +24,23 @@ public class Simulation {
     }
 
     public void runSimulation(){
+        for(int i = 0; i<this.tries; i++){
+            this.bin.incremintBin(dice.tossAndSum());
+        }
+    }
+
+    public void result(){
+        int min = 1*this.dice.getAmntDice();
+        int max = 6*this.dice.getAmntDice();
+        int range = max - min + 1;
+        this.rollResult = "*** \n" +
+                "Simulation of " + this.dice.getAmntDice() +
+                " dice tossed for " + this.tries + " times. \n" +
+                "***\n\n";
+
+        for(int i = 0; i < range; i++){
+            this.rollResult += min+" :    ";
+        }
 
     }
 
