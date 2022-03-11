88147e73019a4b25eefb788e41ca5d7d7ad4c896
DavidCun <99755247+DavidSamCun@users.noreply.github.com>
2022-03-11 13:31:43 -0500

    simulation

--- src/main/java/Simulation.java
+++ src/main/java/Simulation.java
@@ -1,5 +1,29 @@
+
+
 public class Simulation {
 
+    Dice dice;
+    Bins bin;
+    int tries;
 
+    public Simulation(){
+
+        this.dice = new Dice(1);
+        this.bin = new Bins(1, 6);
+
+    }
+
+    public Simulation(int dice, int amountOfTries){
+
+        this.dice = new Dice(dice);
+        this.bin = new Bins (1*dice, 6*dice);
+        this.tries = amountOfTries;
+
+
+    }
+
+    public void runSimulation(){
+
+    }
 
 }
